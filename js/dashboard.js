52552800c05d80749dbfd028d8aa3e6f3a91fe0b
mrln033 <merlin.merzhin.lesage@gmail.com>
2026-02-21 16:38:45 +0100

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -18,7 +18,7 @@ async function loadDashboard() {
 
   try {
 
-    const API_URL = "https://script.google.com/macros/s/AKfycbyd8APwp1AuojSuaetP96Wt7troDtYEDbeiCyD4KTPOey_7Pe1FcS1W_c_J6mVC6P3cLA/exec";
+    const API_URL = "https://script.google.com/macros/s/AKfycby36Jo5ysrBuojfrq_of3r6-R6SM24L-JBxhWHMPGo8Pop8kWv0vgc43QLUdqcwebOR0w/exec";
 
 
 const json = await getApiData(API_URL);
